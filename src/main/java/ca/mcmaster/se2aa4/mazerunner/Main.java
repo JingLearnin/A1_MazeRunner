4242903c8a6b6f515c1b0947e6bf44e0abb2ac45
jingz2 <160545616+JingLearnin@users.noreply.github.com>
2025-01-21 15:37:50 -0500

    Update Main.java

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -10,41 +10,30 @@ import org.apache.logging.log4j.Logger;
 
 public class Main {
 
+    //Logger in init
     private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
-        logger.info("** Starting Maze Runner");
+        logger.info("**** Starting Maze Runner Program ****");
         
         //Create a parser
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = null;
 
         try {
-            System.out.println("**** Reading the maze from file " + args[0]);
-            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                for (int idx = 0; idx < line.length(); idx++) {
-                    if (line.charAt(idx) == '#') {
-                        System.out.print("WALL ");
-                    } else if (line.charAt(idx) == ' ') {
-                        System.out.print("PASS ");
-                    }
-                }
-                System.out.print(System.lineSeparator());
-            }
-        } catch(Exception e) {
-            System.err.println("/!\\ An error has occured /!\\");
-        }
-        System.out.println("**** Computing path");
-        System.out.println("PATH NOT COMPUTED");
-        System.out.println("** End of MazeRunner");
-    }
+            cmd = parser.parse(getParserOption(),args);
+            String fileName = cmd.getOptionValue("i");
+            
 }
-
-private static Options getOption(){
+    }
+private static Options getParserOption(){
     Options options = new Options();
+    
     Option mazeOption = new Option("i", true, "Maze File");
+    //Print error message if there is no input
+    mazeOption.setRequired(true);
+    //adding options to the options object
+    options.addOption(mazeOption);
 
 }  
-
+}
