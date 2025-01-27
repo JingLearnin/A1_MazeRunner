654275b1ede3298d8913229688d6da6ae93bccb2
JingLearnin <jerryzhijing@gmail.com>
2025-01-26 21:22:33 -0500

    Updated Position and Maze classes for t1 functionality

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
@@ -1,33 +1,90 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
-import org.apache.logging.log4j.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Maze {
+    
     private static final Logger logger = LogManager.getLogger();
+    private final List<List<Boolean>> maze = new ArrayList<>();
+
+    private final Position startPos;
+    private final Position endPos;
+
     public Maze(String filePath) throws Exception {
-    BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        logger.debug("Loading maze file from: " + filePath);
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        String blocks;
+        
+        while ((blocks = reader.readLine()) != null) {
+            //A list representing the current row of the maze.
+            List<Boolean> arrayblock = new ArrayList<>();
+            
+            
+            for (int i = 0; i < blocks.length(); i++) {
+                
+                if (blocks.charAt(i) == '#') {
+                    arrayblock.add(true);
+
+                } else if (blocks.charAt(i) == ' ') {
+                    arrayblock.add(false);
+                }
+            }
+            maze.add(arrayblock);
+        }
+        startPos = startPosFinder();
+        endPos = endPosFinder();
+    }
 
     
-    startPos = startPosition();
-    endPos = endPostion();
-
+    private Position startPosFinder() throws Exception {
+        for (int i = 0; i < maze.size(); i++) {
+            Position pos = new Position(0, i);
+            if (isPath(pos) && !isWall(pos)) {
+                return pos;
+            }
+        }
+        throw new Exception("No vaild start position");
     }
-    
 
-
-private Position startPosition () throws Exception {
-    for (int i = 0; i < maze.size; i++){
-        Position pos = new Position(0,i);
-        
-        if 
+    private Position endPosFinder() throws Exception {
+        int lastColumn = maze.getFirst().size() - 1;
+        for (int i = 0; i < maze.size(); i++) {
+            Position pos = new Position(lastColumn, i);
+            if (isPath(pos) && !isWall(pos)) {
+                return pos;
+            }
+        }
+        throw new Exception("No vaild end position");
     }
-}
 
-private Position endPostion() throws Exception{
-    for (int i - 0; i < maze.size; i++){
-        
+    public Boolean isPath(Position pos) {
+        return !maze.get(pos.y()).get(pos.x());
     }
-}
+
+    public Boolean isWall(Position pos){
+        return maze.get(pos.y()).get(pos.x());
+    }
+
+    public Position getStartPos(){
+        return startPos;
+    }
+
+    public Position getEndPos(){
+        return endPos;
+    }
+
+    public int getSizeX() {
+        return this.maze.getFirst().size();
+    }
+
+    public int getSizeY() {
+        return this.maze.size();
+    }
+
 }
--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Position.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Position.java
@@ -1,12 +1,6 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 public record Position (int x, int y) {
-    public Position move(Direction direction){
-        switch (direction){
-            case UP ->{
-                return Position (0,1)
-            }
-        }
-    }
+    
     
 }
