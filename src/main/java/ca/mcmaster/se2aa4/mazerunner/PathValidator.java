5a1c17f2da5d1458dcf3c48203395374c034ffc5
JingLearnin <jerryzhijing@gmail.com>
2025-02-10 20:37:01 -0500

    Final Product  - adding feature #19

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/PathValidator.java
@@ -0,0 +1,11 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+
+
+public class PathValidator {
+    private static final Logger logger = LogManager.getLogger();
+}
